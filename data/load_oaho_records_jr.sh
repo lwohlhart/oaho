20ca4e4cbdcff0a979e3417e5d35b703f8d846f8
Lucas Wohlhart <lucas.wohlhart@joanneum.at>
2019-07-15 16:39:48 +0200

    data copy script get all available tfrecords with gvfs-ls

--- data/load_oaho_records_jr.sh
+++ data/load_oaho_records_jr.sh
@@ -10,8 +10,11 @@ fi
 echo -e "\e[33mHi, please login at rob_file with your JR credentials (Domain: jr1)\e[0m"
 gvfs-mount smb://rzjna01/rob_file/
 
-gvfs-copy -i -p smb://rzjna01/rob_file/staff/wol/oaho/data/oaho_synth_train.tfrecord .
-gvfs-copy -i -p smb://rzjna01/rob_file/staff/wol/oaho/data/oaho_synth_val.tfrecord .
-gvfs-copy -i -p smb://rzjna01/rob_file/staff/wol/oaho/data/oaho_synth_test.tfrecord .
+RECORDS=$(gvfs-ls smb://rzjna01/rob_file/staff/wol/oaho/data/)
+for record in $RECORDS;
+do
+  gvfs-copy -i -p smb://rzjna01/rob_file/staff/wol/oaho/data/$record .
+done
 
 #gvfs-mount -u smb://rzjna01/rob_file/
+
