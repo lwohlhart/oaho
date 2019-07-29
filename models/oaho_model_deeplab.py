9b33c0999643bf6d1a51b7df93e9d3be52b2485a
Lucas Wohlhart <lucas.wohlhart@joanneum.at>
2019-07-29 19:19:52 +0200

    experimentally use deeplab multi_scale_logits

--- models/oaho_model_deeplab.py
+++ models/oaho_model_deeplab.py
@@ -28,12 +28,13 @@ class OAHOModelDeeplab(OAHOModel):
         if 'resnet' in config['model_variant'] or 'xception' in config['model_variant']:
             tf.flags.FLAGS.decoder_output_stride = [4]
             atrous_rates = [6, 12, 18]
-        elif 'mobilenet' in config['model_variant']:
+        elif 'mobilenet' in config['model_variant']: 
             tf.flags.FLAGS.decoder_output_stride = None
             atrous_rates = None
 
+        outputs_to_num_classes = {'semantic': 4, 'pos': 1, 'angle_sin': 1, 'angle_cos': 1, 'width': 1}
 
-        self.model_options = dlc.ModelOptions(4, atrous_rates=atrous_rates, crop_size=[480,640])
+        self.model_options = dlc.ModelOptions(outputs_to_num_classes, atrous_rates=atrous_rates, crop_size=[480,640])
 
 
     def _create_model(self, x: tf.Tensor, is_training: bool) -> tf.Tensor:
@@ -54,36 +55,51 @@ class OAHOModelDeeplab(OAHOModel):
 
         input_tensor = x
         x = tf.keras.layers.concatenate([x,x,x])
-        features_encoder, endpoints = dlm.extract_features(x, model_options,
-            weight_decay=weight_decay,
-            reuse=reuse,
-            is_training=is_training,
-            fine_tune_batch_norm=fine_tune_batch_norm,
-            nas_training_hyper_parameters=nas_training_hyper_parameters)
-        features_decoder = dlm.refine_by_decoder(features_encoder,
-            endpoints,
-            crop_size=model_options.crop_size,
-            decoder_output_stride=model_options.decoder_output_stride,
-            model_variant=model_options.model_variant,
-            weight_decay=weight_decay,
-            reuse=reuse,
-            is_training=is_training,
-            fine_tune_batch_norm=fine_tune_batch_norm,
-            use_bounded_activation=model_options.use_bounded_activation
+        # features_encoder, endpoints = dlm.extract_features(x, model_options,
+        #     weight_decay=weight_decay,
+        #     reuse=reuse,
+        #     is_training=is_training,
+        #     fine_tune_batch_norm=fine_tune_batch_norm,
+        #     nas_training_hyper_parameters=nas_training_hyper_parameters)
+        # features_decoder = dlm.refine_by_decoder(features_encoder,
+        #     endpoints,
+        #     crop_size=model_options.crop_size,
+        #     decoder_output_stride=model_options.decoder_output_stride,
+        #     model_variant=model_options.model_variant,
+        #     weight_decay=weight_decay,
+        #     reuse=reuse,
+        #     is_training=is_training,
+        #     fine_tune_batch_norm=fine_tune_batch_norm,
+        #     use_bounded_activation=model_options.use_bounded_activation
+        #     )
+
+
+        # x = utils.resize_bilinear(features_decoder, [480,640], features_decoder.dtype)
+        outputs_to_scales_to_logits = dlm.multi_scale_logits(x, 
+            model_options, 
+            None, 
+            weight_decay=weight_decay,  
+            is_training=is_training,  
+            fine_tune_batch_norm=fine_tune_batch_norm,  
+            nas_training_hyper_parameters=nas_training_hyper_parameters
             )
 
+        seg_output = outputs_to_scales_to_logits['semantic']['merged_logits']
 
-        x = utils.resize_bilinear(features_decoder, [480,640], features_decoder.dtype)
-
+        pos_output = outputs_to_scales_to_logits['pos']['merged_logits']
+        cos_output = outputs_to_scales_to_logits['angle_sin']['merged_logits']
+        sin_output = outputs_to_scales_to_logits['angle_cos']['merged_logits']
+        width_output = outputs_to_scales_to_logits['width']['merged_logits']
+        
 
         # ===================================================================================================
         # Output layers
-        seg_head = kl.Conv2D(32, kernel_size=2, padding='same', name='seg_head_1', activation='relu')(x)
-        seg_output = kl.Conv2D(4, kernel_size=2, padding='same', name='seg_out')(seg_head)
+        # seg_head = kl.Conv2D(32, kernel_size=2, padding='same', name='seg_head_1', activation='relu')(x)
+        # seg_output = kl.Conv2D(4, kernel_size=2, padding='same', name='seg_out')(seg_head)
 
-        grasp_head = kl.Conv2D(32, kernel_size=2, padding='same', name='grasp_head_1', activation='relu')(x)
-        pos_output = kl.Conv2D(1, kernel_size=2, padding='same', name='pos_out')(grasp_head)
-        cos_output = kl.Conv2D(1, kernel_size=2, padding='same', name='cos_out')(grasp_head)
-        sin_output = kl.Conv2D(1, kernel_size=2, padding='same', name='sin_out')(grasp_head)
-        width_output = kl.Conv2D(1, kernel_size=2, padding='same', name='width_out')(grasp_head)
+        # grasp_head = kl.Conv2D(32, kernel_size=2, padding='same', name='grasp_head_1', activation='relu')(x)
+        # pos_output = kl.Conv2D(1, kernel_size=2, padding='same', name='pos_out')(grasp_head)
+        # cos_output = kl.Conv2D(1, kernel_size=2, padding='same', name='cos_out')(grasp_head)
+        # sin_output = kl.Conv2D(1, kernel_size=2, padding='same', name='sin_out')(grasp_head)
+        # width_output = kl.Conv2D(1, kernel_size=2, padding='same', name='width_out')(grasp_head)
         return seg_output, pos_output, cos_output, sin_output, width_output
